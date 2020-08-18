111c34fcb605910ae02c2daff9af971176bb3017
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-18 02:22:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const driver = {};
+const driver = {
+  name: 'Sam',
+  age: 30
+};
 function updateDriverWithKeyAndValue(driver,key,value){
   return Object.assign({},driver,{[key]:value});
 }
@@ -15,7 +18,19 @@ console.log(driver);
 console.log(driver2);
 
 function deleteFromDriverByKey(driver,key) {
-  Object.assign({},driver,{[key]:value});
+  const newdriver = Object.assign({},driver);
+  delete newdriver[key];
+  return newdriver;
+}
+const driver3 = deleteFromDriverByKey(driver,'name');
+console.log(driver);
+console.log(driver3);
+
+function destructivelyDeleteFromDriverByKey(driver,key) {
+  Object.assign({},driver);
   delete driver[key];
   return driver;
 }
+const driver4 = destructivelyDeleteFromDriverByKey(driver,'name');
+console.log(driver);
+console.log(driver4);
