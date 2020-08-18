ec834a0c7bb12defd35ba5b412628038aa23cf3e
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-08-18 02:19:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const driver = {};
+function updateDriverWithKeyAndValue(driver,key,value){
+  return Object.assign({},driver,{[key]:value});
+}
+const driver1 = updateDriverWithKeyAndValue(driver,'name','Tom');
+console.log(driver);
+console.log(driver1);
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+  driver[key] = value;
+  return driver;
+}
+const driver2 = updateDriverWithKeyAndValue(driver,'name','John');
+console.log(driver);
+console.log(driver2);
+
+function deleteFromDriverByKey(driver,key) {
+  Object.assign({},driver,{[key]:value});
+  delete driver[key];
+  return driver;
+}
